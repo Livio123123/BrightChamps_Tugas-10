26fc8ce245d76bb0024863d10ff40cec9819c246
Livio123123 <99071165+Livio123123@users.noreply.github.com>
2022-03-07 16:29:44 +0700

    Add files via upload

---
+++ tugas9.py
@@ -0,0 +1,35 @@
+
+#==code==
+
+dictEx={
+    1:'one',
+    2:'two',
+    3:'three',
+    4:'four',
+    5:'five',
+    6:'six',
+    7:'seven',
+    8:'eight',
+    9:'nine',
+}
+
+print(dictEx[6])
+print(dictEx.get(6))
+dictEx[6]="sixth position change"
+print(dictEx)
+print(dictEx.values())
+
+
+
+#==soal==
+# 4. sebutkan perbedaan antara list, tuples dan dictionary
+# list = Dalam bahasa pemrograman Python, struktur data yang paling dasar 
+# tuples = salah satu struktur data di Python yang mampu menyimpan sekumpulan nilai dalam satu variabel.
+# dictionary = stuktur data yang bentuknya seperti kamus
+
+
+#==data==
+
+#dictEx={ 1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine', }
+
+
